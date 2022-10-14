ca1e004cd29fa2d500fc93f93ca931e5737ba208
rateme5stars <dzungardan@gmail.com>
2022-10-14 11:32:23 +0700

    finish

--- google_scraper.py
+++ google_scraper.py
@@ -1,18 +1,9 @@
+from random import uniform
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 import json
-import random
-
-
-def random_sleep_time1():
-    return round(random.random(), 2) * 5
-
-
-def random_sleep_time2():
-    return round(random.random(), 2) * 10
-
 
 with open('celeb_name.txt', 'r') as celeb_file:
     names = celeb_file.readlines()
@@ -23,43 +14,57 @@ for i, name in enumerate(names):
 driver = webdriver.Chrome(ChromeDriverManager().install())
 graph = list()
 
-iter_num = 0
+start_process = time.time()
 for name in names:
-    print(name)
+    start_celeb = time.time()
     cluster = {}
     neighbor_list = list()
     driver.get(f"https://www.google.com/search?q={name}")
 
-    time.sleep(random_sleep_time2())
-    job = driver.find_element(By.XPATH, value="//div[@class='wx62f PZPZlf x7XAkb']").text
+    time.sleep(uniform(5, 6))
 
-    time.sleep(random_sleep_time1())
-    see_more_bt = driver.find_element(By.XPATH, value='//g-more-link/../../a')
-    see_more_bt.click()
+    try:
+        print(f'--- Getting {name} Information ---')
+        job = driver.find_element(By.XPATH, value='//div[@class="wx62f PZPZlf x7XAkb"]').text
+        see_more_bt = driver.find_element(By.XPATH, value="//g-more-link/../../a")
+        see_more_bt.click()
+        time.sleep(uniform(2, 3))
+        see_more_list = driver.find_elements(By.XPATH, value="//g-scrolling-carousel/div/div/a")
+    except Exception as error:
+        job = 'Unknown'
+        print(f'There is an error with {name}')
+        continue
 
-    time.sleep(random_sleep_time1())
-    see_more_list = driver.find_elements(By.XPATH, value='//g-scrolling-carousel/div/div/a')
-
+    print('--- Getting The Relevants Information ---')
     for idx, person in enumerate(see_more_list):
         neighbor = {}
-        relevant_name = person.get_attribute('aria-label')
-
-        time.sleep(random_sleep_time2())
-        relevant_tab = driver.find_element(By.XPATH, value=f"//g-scrolling-carousel/div/div/a[contains(@aria-label, '{relevant_name}')]")
-        relevant_tab.click()
-        time.sleep(random_sleep_time1())
-        title = driver.find_element(By.XPATH, value="//div[@class='wx62f PZPZlf x7XAkb']").text
+        try:
+            relevant_name = person.get_attribute('aria-label')
+            relevant_tab = driver.find_element(By.XPATH, value=f'//g-scrolling-carousel/div/div/a[contains(@aria-label, "{relevant_name}")]')
+            relevant_tab.click()
+            time.sleep(uniform(2, 3))
+            title = driver.find_element(By.XPATH, value='//div[@class="wx62f PZPZlf x7XAkb"]').text
+        except Exception as error:
+            title = 'Unknown'
+            print(f'There is an error with {relevant_name}')
+            continue
 
         neighbor['job'] = title
         neighbor['rank'] = idx + 1
         neighbor['neighbor_name'] = relevant_name
         neighbor_list.append(neighbor)
+
     cluster['name'] = name
     cluster['job'] = job
     cluster['connections'] = neighbor_list
     graph.append(cluster)
 
-with open('graph1.json', 'w', encoding='utf8') as fp:
-    json.dump(graph, fp, ensure_ascii=False)
+    end_celeb = time.time()
+    print(f'Done: {end_celeb-start_celeb}\n')
 
+with open('graph2.json', 'w', encoding='utf8') as fp:
+    json.dump(graph, fp, ensure_ascii=False)
+end_process = time.time()
+
+print(f'Crawling duration: {end_process - start_process}')
 driver.quit()
